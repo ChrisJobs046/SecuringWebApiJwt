a1268fa5bbcc9dff905ee1639281c9a079dda65c
ChrisJobs046 <ChrisJobs046@gmail.com>
2021-05-28 00:29:32 -0400

    Terminamos el controlador que hace varias validaciones y llama a Login🚶‍♀️🚶‍♀️

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,4 +1,7 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SecuringWebApiJwt.Interfaces;
+using SecuringWebApiJwt.Requests;
 
 namespace SecuringWebApiJwt.Controllers
 {
@@ -8,8 +11,35 @@ namespace SecuringWebApiJwt.Controllers
     junto con otros detalles del cliente si el proceso de inicio de sesión se realizó correctamente de lo 
     contrario devolverá 400 solicitudes incorrectas
     */
+
+    [Route("api/[controller]")]
+    [ApiController]
     public class CustomersController: ControllerBase
     {
-        
+        private readonly ICustomerService _customService;
+
+        public CustomersController(ICustomerService customer){
+
+            this._customService = customer;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult> Login(LoginRequest loginRequest){
+
+            if(loginRequest == null || string.IsNullOrEmpty(loginRequest.Username) || string.IsNullOrEmpty(loginRequest.Password)){
+
+                return BadRequest("Missing login details");
+            }
+
+            var loginResponse = await _customService.Login(loginRequest);
+
+            if (loginResponse == null)
+            {
+                return BadRequest($"Invalid credentials");
+            }
+            
+            return Ok(loginResponse);
+        }
     }
 }
