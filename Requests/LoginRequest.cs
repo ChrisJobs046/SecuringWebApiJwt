6c6f1067948c564ec4252d919503e94bc7c1d463
ChrisJobs046 <ChrisJobs046@gmail.com>
2021-05-27 22:47:58 -0400

    Clase que representara los campos de inicio de sesion😁😁

---
+++ Requests/LoginRequest.cs
@@ -0,0 +1,9 @@
+namespace SecuringWebApiJwt.Requests
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
